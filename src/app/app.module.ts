f198025a67b9ec555555ea7ca8fafc485114d989
Valeria-27 <delgadillo.escobar.valeria27@gmail.com>
2022-11-02 10:30:48 -0400

    Agregando dependecias a la develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,52 +5,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
 import { LoadScriptsService } from './load-scripts.service';
-<<<<<<< HEAD
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './views/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //Angular Material
 import { MatSliderModule } from '@angular/material/slider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import { SupplierComponent } from './components/supplier/supplier.component';
+import { SupplierComponent } from './views/supplier/supplier.component';
 import { MatTableModule } from '@angular/material/table';
-=======
 import { RegisterComponent } from './views/register/register.component';
 import { PasswordRecoveryComponent } from './views/recovery_password/password-recovery/password-recovery.component';
 import { UpdatePasswordComponent } from './views/recovery_password/update-password/update-password.component';
->>>>>>> 10689563eab5f32fb2c760e8c390c381ccb9f50e
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-<<<<<<< HEAD
     LoginComponent,
-    SupplierComponent
-=======
+    SupplierComponent,
     RegisterComponent,
     PasswordRecoveryComponent,
     UpdatePasswordComponent
->>>>>>> 10689563eab5f32fb2c760e8c390c381ccb9f50e
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     MatSliderModule,
     MatFormFieldModule,
     FormsModule,
     MatInputModule,
     MatButtonModule,
-    MatTableModule
-    
-=======
-    FormsModule,
+    MatTableModule,
+    MatToolbarModule,
     ReactiveFormsModule
->>>>>>> 10689563eab5f32fb2c760e8c390c381ccb9f50e
   ],
   providers: [
     LoadScriptsService
