fe76668ca6fc93d96192121b22b8bc17694a18c5
Nikypm <pmn0028257@est.univalle.edu>
2022-11-01 23:35:56 -0400

    adding password recovery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
 import { LoadScriptsService } from './load-scripts.service';
 import { RegisterComponent } from './views/register/register.component';
+import { PasswordRecoveryComponent } from './views/recovery_password/password-recovery/password-recovery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    RegisterComponent
+    RegisterComponent,
+    PasswordRecoveryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     LoadScriptsService
