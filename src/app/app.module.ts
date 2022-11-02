4a06ad058024985ac1a625823bcabbe16db7229d
Valeria-27 <delgadillo.escobar.valeria27@gmail.com>
2022-11-02 09:22:24 -0400

    Avances Compras y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
 import { LoadScriptsService } from './load-scripts.service';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//Angular Material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { SupplierComponent } from './components/supplier/supplier.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    LoginComponent,
+    SupplierComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
+    
   ],
   providers: [
     LoadScriptsService
