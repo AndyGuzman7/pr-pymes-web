27ebaad016865c1aceb2d4deb3a5499a782a7460
Nikypm <pmn0028257@est.univalle.edu>
2022-11-02 10:37:43 -0400

    adding warehouse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,29 @@ import { LoadScriptsService } from './load-scripts.service';
 import { RegisterComponent } from './views/register/register.component';
 import { PasswordRecoveryComponent } from './views/recovery_password/password-recovery/password-recovery.component';
 import { UpdatePasswordComponent } from './views/recovery_password/update-password/update-password.component';
+import { AllWarehousesComponent } from './views/warehouses/all-warehouses/all-warehouses.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 
 @NgModule({
   declarations: [
@@ -15,13 +38,34 @@ import { UpdatePasswordComponent } from './views/recovery_password/update-passwo
     TopNavBarComponent,
     RegisterComponent,
     PasswordRecoveryComponent,
-    UpdatePasswordComponent
+    UpdatePasswordComponent,
+    AllWarehousesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
   ],
   providers: [
     LoadScriptsService
