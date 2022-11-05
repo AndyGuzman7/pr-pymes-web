d63a0139f0a83b87c78d778d6632515db7d82c96
LitosCar19 <carlitos.sebu@gmail.com>
2022-11-05 12:25:28 -0400

    Show products iterations (false data product)

--- src/app/components/ventas/ventas.component.ts
+++ src/app/components/ventas/ventas.component.ts
@@ -1,4 +1,6 @@
+import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-ventas',
@@ -9,8 +11,50 @@ export class VentasComponent implements OnInit {
 
   public titulo: string = 'Listado de ventas';
   constructor() { }
-
-  ngOnInit(): void {
+  producto1 : Producto = {
+    idProducto: 1,
+    producto: 'Portatil',
+    descripcion: 'i5 75000QH 1TB SSD CRUSSIAL',
+    cantidad: 2,
+    precio: 1230,
+    subtotal: 1
+  }
+  producto2 : Producto = {
+    idProducto: 2,
+    producto: 'Licuadora PHILIPS',
+    descripcion: 'Modelo K3829 + Trituradora + Envase',
+    cantidad: 2,
+    precio: 240,
+    subtotal: 1
+  }
+  producto3 : Producto = {
+    idProducto: 3,
+    producto: 'Parlante SONY-X2H3',
+    descripcion: 'Sonido envolvente, 5 bocinas',
+    cantidad: 2,
+    precio: 150,
+    subtotal: 1
   }
 
+  productos = [
+    this.producto1,
+    this.producto2,
+    this.producto3
+  ]
+  data = Object.values(this.productos)
+  ngOnInit(): void {
+    //console.table(this.producto1);
+    
+  }
+  
+  public deleteOneProductList (id: number) {
+    this.data = this.data.filter((item) => item.idProducto !== id)
+  }
+
+
+  
+  
+
+
 }
+
