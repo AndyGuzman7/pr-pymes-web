09eb6fc77b0f593f5cd599be663796308c2f2e7a
LitosCar19 <carlitos.sebu@gmail.com>
2022-11-05 12:17:24 -0400

    Show products iterations (false data product)

---
+++ src/app/models/producto.ts
@@ -0,0 +1,10 @@
+import { VariableBinding } from "@angular/compiler";
+
+export class Producto {
+    idProducto: number;
+    producto:string;
+    descripcion:string;
+    cantidad:number;
+    precio:number;
+    subtotal: number;
+}
