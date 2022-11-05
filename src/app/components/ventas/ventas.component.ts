c0ff90cfd92246654da2a605c5cca74952945be8
LitosCar19 <carlitos.sebu@gmail.com>
2022-11-05 18:16:34 -0400

    Fron-end improvements and details

--- src/app/components/ventas/ventas.component.ts
+++ src/app/components/ventas/ventas.component.ts
@@ -1,7 +1,8 @@
 import { PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
-
+import {LoadScriptsService} from 'src/app/load-scripts.service'
+declare var js;
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
@@ -10,7 +11,14 @@ import { Producto } from 'src/app/models/producto';
 export class VentasComponent implements OnInit {
 
   public titulo: string = 'Listado de ventas';
-  constructor() { }
+  constructor(private _LoadScripts:LoadScriptsService) { 
+    _LoadScripts.Load(["accordion"]);
+  }
+
+   ngOnInit(): void {
+    //console.table(this.producto1);
+    
+  }
   producto1 : Producto = {
     idProducto: 1,
     producto: 'Portatil',
@@ -42,18 +50,12 @@ export class VentasComponent implements OnInit {
     this.producto3
   ]
   data = Object.values(this.productos)
-  ngOnInit(): void {
-    //console.table(this.producto1);
-    
-  }
+  
   
   public deleteOneProductList (id: number) {
     this.data = this.data.filter((item) => item.idProducto !== id)
   }
 
-
-  
-  
 
 
 }
