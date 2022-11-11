c6591be289fff57e1f42cffa79d6992929022fe4
PedroAndresCondeValdez <pedroandrescondevaldez@gmail.com>
2022-11-11 11:12:49 -0400

    Validacion del formulario de Clientes

--- src/assets/js/accordion.js
+++ src/assets/js/accordion.js
@@ -33,3 +33,121 @@ $("#btnCloseModalProduct").click(() => {
   $("#modalRegisterProduct").modal('toggle');
 })
 
+
+//#region  VALIDACION FORMULARIOS
+const campos ={
+  nombre: false,
+  apellido: false,
+  nit: false,
+  correo: false
+}
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{1,3}$/,
+  //correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+const validarFormulario = (e) => {
+  switch (e.target.name){
+  case "correo":
+    if (expresiones.correo.test(e.target.value)) {
+      ColorCorreoBien();
+      campos['correo'] = true;
+    }else{
+      ColorCorreoMal();
+      campos['correo'] = false;
+    }
+  break;
+  case "nit":
+    if (expresiones.telefono.test(e.target.value)) {
+      ColorNitBien();
+      campos['nit']= true;
+    }else{
+      ColorNitMal();
+      campos['correo'] = false;
+    }
+  break;
+  case "apellido":
+    if (expresiones.nombre.test(e.target.value)) {
+      ColorApellidoBien();
+      campos['apellido']= true;
+    }else{
+      ColorApellidoMal();
+      campos['correo'] = false;
+    }
+  break;
+  case "nombre":
+    if (expresiones.nombre.test(e.target.value)) {
+      //console.log(e.target.value);
+      ColorNombreBien();
+      campos['nombre']= true;
+    }else{
+      //document.getElementById('validar').classList.add('formulario__grupo-correcto');
+      //alert('El campo de Nombre tiene caracteres indebidos');
+      ColorNombreMal();
+      campos['correo'] = false;
+    }
+  break;
+  }
+}
+inputs.forEach((input) =>{
+  input.addEventListener('keyup',validarFormulario);
+  input.addEventListener('blur',validarFormulario);
+});
+
+formulario.addEventListener('button',(e) =>{
+  e.preventDefault();
+  if(campos['nombre'] && campos['apellido'] && campos['nit'] , campos['correo']){
+    formulario.reset();
+  }
+});
+
+//#endregion
+
+//#region Colores Validacion
+function ColorNombreMal() {
+
+  document.getElementById("validarNombre").style.color = "red";
+
+}
+function ColorNombreBien() {
+
+  document.getElementById("validarNombre").style.color = "green";
+
+}
+function ColorApellidoMal() {
+
+  document.getElementById("validarApellido").style.color = "red";
+
+}
+function ColorApellidoBien() {
+
+  document.getElementById("validarApellido").style.color = "green";
+
+}
+function ColorNitMal() {
+
+  document.getElementById("validarNit").style.color = "red";
+
+}
+function ColorNitBien() {
+
+  document.getElementById("validarNit").style.color = "green";
+
+}
+function ColorCorreoMal() {
+
+  document.getElementById("validarCorreo").style.color = "red";
+
+}
+function ColorCorreoBien() {
+
+  document.getElementById("validarCorreo").style.color = "green";
+
+}
+//#endregion
+
