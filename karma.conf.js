c5755dd6d60f138522e6516fbac886466729c4b6
axe1619 <89085938+axe1619@users.noreply.github.com>
2022-11-18 15:47:57 -0400

    Lista de registros y filtro de registros

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pr-pymes-web'),
+      dir: require('path').join(__dirname, './coverage/facturacion'),
       subdir: '.',
       reporters: [
         { type: 'html' },
